404b9afac0cdf39e8e1e47735b52e232a573b3e7
Song Liu <liusong.me@gmail.com>
2021-02-20 17:23:57 +0800

    see  #5; see #3

--- song_ros_control/include/song_ros_control/A1Controller.h
+++ song_ros_control/include/song_ros_control/A1Controller.h
@@ -5,24 +5,45 @@
 #ifndef SONG_ROS_CONTROL_A1CONTROLLER_H
 #define SONG_ROS_CONTROL_A1CONTROLLER_H
 
+#include <ros/node_handle.h>
+#include <urdf/model.h>
+#include <memory>
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <realtime_tools/realtime_buffer.h>
 
 #include <song_msgs/MotorCmd.h>
+#include <song_msgs/MotorState.h>
 
+#include <pluginlib/class_list_macros.h>
 
 class A1Controller: public controller_interface::Controller<hardware_interface::EffortJointInterface>
 {
 public:
     A1Controller();
-    ~A1Controller();
-    virtual bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
-    virtual void starting(const ros::Time& time);
-    virtual void update(const ros::Time& time, const ros::Duration& period);
-    virtual void stopping();
+    ~A1Controller() override;
+    bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n) override;
+    void starting(const ros::Time& time) override;
+    void update(const ros::Time& time, const ros::Duration& period) override;
+    virtual void stopping(){};
+
+    void cmd_cb(const song_msgs::MotorCmdConstPtr& msg);
 
 private:
-    song_msgs::MotorCmd last_cmd;
+    std::string name_space;
+
+    song_msgs::MotorCmd last_cmd_;
+    song_msgs::MotorState last_state_;
+
+    ros::Subscriber sub_cmd_;
+
+    std::unique_ptr<realtime_tools::RealtimePublisher<song_msgs::MotorState>> robot_status_pub_;
+    std::vector<std::string> joint_name_list_;
+    std::vector<hardware_interface::JointHandle> joint_list_;
+    std::map<std::string, int> joint_name_to_index_;
+
+    realtime_tools::RealtimeBuffer<song_msgs::MotorCmd> command_;
 
 };
 
--- song_ros_control/src/A1Controller.cpp
+++ song_ros_control/src/A1Controller.cpp
@@ -3,9 +3,12 @@
 //
 
 #include "song_ros_control/A1Controller.h"
+
+#include <memory>
 A1Controller::A1Controller()
 {
-    memset(&last_cmd, 0, sizeof(song_msgs::MotorCmd));
+    memset(&last_cmd_, 0, sizeof(song_msgs::MotorCmd));
+    memset(&last_state_, 0, sizeof(song_msgs::MotorState));
 }
 
 A1Controller::~A1Controller()
@@ -15,8 +18,79 @@ A1Controller::~A1Controller()
 
 bool A1Controller::init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n)
 {
+    name_space = n.getNamespace();
 
+    urdf::Model urdf; // Get URDF info about joint
+    if (!urdf.initParamWithNodeHandle("robot_description", n)){
+        ROS_ERROR("Failed to parse urdf file");
+        return false;
+    }
+
+    n.getParam("joint_name_list", joint_name_list_);
+
+    for (int i = 0; i < joint_name_list_.size(); ++i) {
+        ROS_INFO_STREAM(joint_name_list_[i] << " \n ");
+        joint_name_to_index_[joint_name_list_[i]] = i;
+
+        urdf::JointConstSharedPtr joint_urdf = urdf.getJoint(joint_name_list_[i]);
+        if (!joint_urdf) {
+            ROS_ERROR("Could not find joint '%s' in urdf", joint_name_list_[i].c_str());
+            return false;
+        }
+        hardware_interface::JointHandle joint = robot->getHandle(joint_name_list_[i]);
+        joint_list_.push_back(joint);
+    }
+
+    sub_cmd_ = n.subscribe("command", 20, &A1Controller::cmd_cb, this);
+
+    robot_status_pub_ = std::make_unique<realtime_tools::RealtimePublisher<song_msgs::MotorState>>(
+            n, name_space + "/state", 1);
+    return true;
 }
 
+void A1Controller::starting(const ros::Time& time)
+{
+    for (int i = 0; i < joint_name_list_.size(); ++i) {
+        last_cmd_.tau[i] = 0.0;
+        last_cmd_.joint_name[i] = joint_name_list_[i];
+        last_state_.q[i] = 0.0;
+        last_state_.dq[i] = 0.0;
+        last_state_.tau[i] = 0.0;
+        last_state_.joint_name[i] = joint_name_list_[i];
+    }
+    command_.initRT(last_cmd_);
+}
 
+void A1Controller::cmd_cb(const song_msgs::MotorCmdConstPtr& msg)
+{
+    last_cmd_.tau = msg->tau;
+    last_cmd_.joint_name = msg->joint_name;
+
+    command_.writeFromNonRT(last_cmd_);
+}
+
+void A1Controller::update(const ros::Time& time, const ros::Duration& period)
+{
+    last_cmd_ = *(command_.readFromRT());
+
+    for (int i = 0; i != 12; ++i){
+        joint_list_[joint_name_to_index_[last_cmd_.joint_name[i]]].setCommand(last_cmd_.tau[i]);
+    }
+
+    if(robot_status_pub_ && robot_status_pub_->trylock()){
+        for (int i = 0; i != 12; ++i){
+            last_state_.joint_name[i] = joint_name_list_[i];
+            last_state_.q[i] = joint_list_[i].getPosition();
+            last_state_.dq[i] = joint_list_[i].getVelocity();
+            last_state_.tau[i] = joint_list_[i].getEffort();
+        }
+        robot_status_pub_->msg_.q = last_state_.q;
+        robot_status_pub_->msg_.dq = last_state_.dq;
+        robot_status_pub_->msg_.tau = last_state_.tau;
+        robot_status_pub_->msg_.stamp = time;
+        robot_status_pub_->unlockAndPublish();
+    }
+}
+
+PLUGINLIB_EXPORT_CLASS(A1Controller, controller_interface::ControllerBase);
 
